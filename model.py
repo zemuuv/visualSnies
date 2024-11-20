16fe62844bd1fabac88a0640951fa5c995420039
zemuuv <samuvarg@gmail.com>
2024-11-20 02:21:06 -0500

    se subio informacion completa a la base de datos

--- model.py
+++ model.py
@@ -74,7 +74,8 @@ class Database:
     def create_table_graduados(self):
         self.cursor.execute('''
             CREATE TABLE Tabla_Graduados (
-            ID_graduado INT PRIMARY KEY ,
+            ID INTEGER PRIMARY KEY,
+            ID_graduado INTEGER ,
             Fecha_Graduacion DATE,
             ID_Institucion INT,
             ID_Departamento INT,
@@ -89,7 +90,8 @@ class Database:
     def create_table_admitidos(self):
         self.cursor.execute('''
             CREATE TABLE Tabla_Admitidos (
-            ID_Admitidos INT PRIMARY KEY ,
+            ID INTEGER PRIMARY KEY ,
+            ID_ADMITIDOS INTEGER,
             Fecha_Admitidos DATE,
             ID_Institucion INT,
             ID_Metodologia INT,
@@ -102,7 +104,8 @@ class Database:
     def create_table_inscritos(self):
         self.cursor.execute('''
             CREATE TABLE Tabla_Inscritos (
-            ID_Inscritos INT PRIMARY KEY ,
+            ID INTEGER PRIMARY KEY,
+            ID_Inscritos INT,
             Fecha_Inscripcion DATE,
             ID_Sexo INT,
             ID_Formacion INT,
@@ -118,7 +121,8 @@ class Database:
     def create_table_matriculados(self):
         self.cursor.execute('''
             CREATE TABLE Tabla_Matriculados (
-            ID_Matriculados INT PRIMARY KEY,
+            ID INTEGER PRIMARY KEY,
+            ID_Matriculados INT ,
             ID_Programa_Academico INT,
             ID_Institucion INT,
             ID_Año INT,
@@ -138,82 +142,110 @@ class Database:
 
     def insert_acreditacion(self, df, columna_estado):
         df = df[columna_estado].drop_duplicates()
-        df[columna_estado[0]] = df[columna_estado[0]].astype(int)
-        df.rename(columns={columnas[0]: 'ID_Acreditacion', columnas[1]: 'Nombre_Departamento'}, inplace=True)   
+        df[columna_estado[0]] = pd.to_numeric(df[columna_estado[0]], errors='coerce').fillna(0).astype(int)
+        df.rename(columns={columna_estado[0]: 'ID_Acreditacion', columna_estado[1]: 'Estado_Acreditacion'}, inplace=True)   
         df.to_sql('Dimension_Acreditacion', self.conn, if_exists='append', index=False)
         self.conn.commit()
 
-    def insert_institucion(self, df, columna_nombre):
-        df = df[[columna_nombre]].drop_duplicates()
-        df.rename(columns={columna_nombre: 'Nombre_Institucion'}, inplace=True)
-        df.to_sql('Dimension_Institucion', self.conn, if_exists='append', index=False)
+    def insert_institucion(self, df, columnas):
+        df = df[columnas].drop_duplicates()
+        df[columnas[0]] = df[columnas[0]].astype(int)    
+        df.rename(columns={columnas[0]: 'ID_Institucion', columnas[1]: 'Nombre_Institucion'}, inplace=True)   
+        df.to_sql('Dimension_Institucion', self.conn, if_exists='append', index=False)  
         self.conn.commit()
 
-    def insert_metodologia(self, df, columna_tipo):
-        df = df[[columna_tipo]].drop_duplicates()
-        df.rename(columns={columna_tipo: 'Tipo_Metodologia'}, inplace=True)
-        df.to_sql('Dimension_Metodologia', self.conn, if_exists='append', index=False)
+    def insert_metodologia(self, df, columnas):
+        df = df[columnas].drop_duplicates()
+        df[columnas[0]] = df[columnas[0]].astype(int)    
+        df.rename(columns={columnas[0]: 'ID_Metodologia', columnas[1]: 'Tipo_Metodologia'}, inplace=True)   
+        df.to_sql('Dimension_Metodologia', self.conn, if_exists='append', index=False)  
         self.conn.commit()
 
-    def insert_programa_academico(self, df, columna_nombre):
-        df = df[[columna_nombre]].drop_duplicates()
-        df.rename(columns={columna_nombre: 'Nombre_Programa_Academico'}, inplace=True)
-        df.to_sql('Dimension_Programa_Academico', self.conn, if_exists='append', index=False)
+    def insert_programa_academico(self, df, columnas):
+        df = df[columnas].drop_duplicates()
+        df[columnas[0]] = df[columnas[0]].astype(int)    
+        df.rename(columns={columnas[0]: 'ID_Programa_Academico', columnas[1]: 'Nombre_Programa_Academico'}, inplace=True)   
+        df.to_sql('Dimension_Academico', self.conn, if_exists='append', index=False)  
         self.conn.commit()
     
-    def insert_sexo(self, df, columna_sexo):
-        df = df[[columna_sexo]].drop_duplicates()
-        df.rename(columns={columna_sexo: 'Sexo'}, inplace=True)
-        df.to_sql('Dimension_Sexo', self.conn, if_exists='append', index=False)
-        self.conn.commit()
-
-    def insert_formacion(self, df, columna_formacion):
-        df = df[[columna_formacion]].drop_duplicates()
-        df.rename(columns={columna_formacion: 'Formacion'}, inplace=True)
-        df.to_sql('Dimension_Formacion', self.conn, if_exists='append', index=False)
-        self.conn.commit()
-
-    def insert_graduados(self, df, columnas):
+    def insert_sexo(self, df, columnas):
         df = df[columnas].drop_duplicates()
-        df.rename(columns={
-            columnas[0]: 'Fecha_Graduacion',
-            columnas[1]: 'ID_Institucion',
-            columnas[2]: 'ID_Departamento',
-            columnas[3]: 'ID_Acreditacion'
-        }, inplace=True)
-        df.to_sql('Tabla_Graduados', self.conn, if_exists='append', index=False)
+        df[columnas[0]] = df[columnas[0]].astype(int)    
+        df.rename(columns={columnas[0]: 'ID_sexo', columnas[1]: 'Sexo'}, inplace=True)   
+        df.to_sql('Dimension_Sexo', self.conn, if_exists='append', index=False)  
         self.conn.commit()
 
-    def insert_admitidos(self, df, columnas):
+    def insert_formacion(self, df, columnas):
         df = df[columnas].drop_duplicates()
+        df[columnas[0]] = df[columnas[0]].astype(int)    
+        df.rename(columns={columnas[0]: 'ID_Formacion', columnas[1]: 'Formacion'}, inplace=True)   
+        df.to_sql('Dimension_Formacion', self.conn, if_exists='append', index=False)  
+        self.conn.commit()
+    
+    def insert_snies_fact(self, df, columnas):
+    # Renombrar las columnas para que coincidan con los nombres esperados en la base de datos
         df.rename(columns={
-            columnas[0]: 'Fecha_Admitidos',
-            columnas[1]: 'ID_Institucion',
-            columnas[2]: 'ID_Metodologia'
+            columnas[0]: 'ID_Admitidos',
+            columnas[1]: 'Fecha_Admitidos',
+            columnas[2]: 'ID_Institucion',
+            columnas[3]: 'ID_Metodologia'
         }, inplace=True)
+        
+        # Insertar los datos en la base de datos en la tabla SNIES_FACT
         df.to_sql('Tabla_Admitidos', self.conn, if_exists='append', index=False)
+        
+        # Confirmar los cambios en la base de datos
         self.conn.commit()
 
-    def insert_inscritos(self, df, columnas):
-        df = df[columnas].drop_duplicates()
+    def insert_graduados_fact(self, df, columnas):
+        # Renombrar las columnas para que coincidan con los nombres esperados en la base de datos
         df.rename(columns={
-            columnas[0]: 'Fecha_Inscripcion',
-            columnas[1]: 'ID_Sexo',
-            columnas[2]: 'ID_Formacion',
-            columnas[3]: 'ID_Programa_Academico'
+            columnas[0]: 'ID_GRADUADO',
+            columnas[1]: 'FECHA_GRADUACION',
+            columnas[2]: 'ID_INSTITUCION',
+            columnas[3]: 'ID_DEPARTAMENTO',
+            columnas[4]: 'ID_ACREDITACION',
         }, inplace=True)
+
+        # Insertar los datos en la base de datos en la tabla de graduados
+        df.to_sql('Tabla_Graduados', self.conn, if_exists='append', index=False)
+
+        # Confirmar los cambios en la base de datos
+        self.conn.commit()
+
+    def insert_inscritos_fact(self, df, columnas):
+    # Renombrar las columnas para que coincidan con los nombres esperados en la base de datos
+        df.rename(columns={
+            columnas[0]: 'ID_INSCRITOS',
+            columnas[1]: 'FECHA_INSCRIPCION',
+            columnas[2]: 'ID_SEXO',
+            columnas[3]: 'ID_FORMACION',
+            columnas[4]: 'ID_PROGRAMA_ACADEMICO'
+        }, inplace=True)
+
+        # Insertar los datos en la base de datos en la tabla INSCRITOS_FACT
         df.to_sql('Tabla_Inscritos', self.conn, if_exists='append', index=False)
+
+        # Confirmar los cambios en la base de datos
         self.conn.commit()
     
-    def insert_matriculados(self, df, columnas):
-        df = df[columnas].drop_duplicates()
+    def insert_matriculados_fact(self, df, columnas):
+    # Renombrar las columnas para que coincidan con los nombres esperados en la base de datos
         df.rename(columns={
-            columnas[0]: 'ID_Programa_Academico',
-            columnas[1]: 'ID_Institucion',
-            columnas[2]: 'ID_Año'
+            columnas[0]: 'ID_MATRICULADOS',
+            columnas[1]: 'ID_PROGRAMA_ACADEMICO',
+            columnas[2]: 'ID_INSTITUCION',
+            columnas[3]: 'ID_Año'
         }, inplace=True)
+
+        # Insertar los datos en la base de datos en la tabla MATRICULADOS_FACT
         df.to_sql('Tabla_Matriculados', self.conn, if_exists='append', index=False)
+
+        # Confirmar los cambios en la base de datos
         self.conn.commit()
+
+
+
 
 class Cargue:
 
--- preparacion.py
+++ preparacion.py
@@ -3,7 +3,8 @@ from model import Cargue
 import pandas as pd
 
 base_datos = Database('baseDatos.db')
-# base_datos.create_table_Departamento()
+
+#base_datos.create_table_Departamento()
 #base_datos.create_table_acreditacion()
 #base_datos.create_table_institucion()
 #base_datos.create_table_metodologia()
@@ -22,54 +23,148 @@ df_admitidos_2023 = carga.cargue_archivo(nombre_archivo='Admitidos2023.xlsx',hoj
 df_matriculados_2023 = carga.cargue_archivo(nombre_archivo='Matriculados2023.xlsx',hoja=1, encabezado=5, codigo_institucion=institucion)
 df_graduados_2023 = carga.cargue_archivo(nombre_archivo='Graduados2023.xlsx',hoja=1, encabezado=5, codigo_institucion=institucion)
 df_inscritos_2023 = carga.cargue_archivo(nombre_archivo='Inscritos2023.xlsx',hoja=1, encabezado=5, codigo_institucion=institucion)
+
+
+
 '''
 #Cargue de departamentos
 columnas = ['CÓDIGO DEL DEPARTAMENTO (PROGRAMA)', 'DEPARTAMENTO DE OFERTA DEL PROGRAMA']
 base_datos.insert_departamento(df_graduados_2023,columnas)
-'''
+
 #Cargue de acreditacion
 columnas_acreditacion = ['CÓDIGO SNIES DEL PROGRAMA', 'PROGRAMA ACREDITADO']
 base_datos.insert_acreditacion(df_graduados_2023,columnas_acreditacion)
 
 #Cargue de institucion
 columnas_acreditacion = ['CÓDIGO DE LA INSTITUCIÓN', 'INSTITUCIÓN DE EDUCACIÓN SUPERIOR (IES)']
-base_datos.insert_acreditacion(df_graduados_2023,columnas_acreditacion)
-base_datos.insert_acreditacion(df_admitidos_2023,columnas_acreditacion)
-base_datos.insert_acreditacion(df_matriculados_2023,columnas_acreditacion)
+base_datos.insert_institucion(df_graduados_2023,columnas_acreditacion)
 
 #Cargue de metodologia
 columnas_acreditacion = ['ID MODALIDAD', 'MODALIDAD']
-base_datos.insert_acreditacion(df_admitidos_2023,columnas_acreditacion)
+base_datos.insert_metodologia(df_admitidos_2023,columnas_acreditacion)
 
 #Cargue de Programa_academico
 columnas_acreditacion = ['CÓDIGO SNIES DEL PROGRAMA', 'PROGRAMA ACADÉMICO']
-base_datos.insert_acreditacion(df_matriculados_2023,columnas_acreditacion)
-base_datos.insert_acreditacion(df_inscritos_2023,columnas_acreditacion)
+base_datos.insert_programa_academico(df_matriculados_2023,columnas_acreditacion)
 
 #Cargue de sexo
 columnas_acreditacion = ['ID SEXO', 'SEXO']
-base_datos.insert_acreditacion(df_graduados_2023,columnas_acreditacion)
+base_datos.insert_sexo(df_graduados_2023,columnas_acreditacion)
 
 #Cargue de formacion
 columnas_acreditacion = ['ID NIVEL DE FORMACIÓN', 'NIVEL DE FORMACIÓN']
-base_datos.insert_acreditacion(df_graduados_2023,columnas_acreditacion)
-'''
-#Cargue de SNIES_FACT
-columnas_admitidos = ['CÓDIGO DE LA INSTITUCIÓN','CÓDIGO SNIES DEL PROGRAMA','ID ÁREA','ID SEXO','AÑO','SEMESTRE','ADMITIDOS']
+base_datos.insert_formacion(df_graduados_2023,columnas_acreditacion)
+
+
+# Filtrar el DataFrame para que solo contenga las columnas necesarias de admitidos
+columnas_admitidos = ['CÓDIGO DE LA INSTITUCIÓN', 'ID MODALIDAD', 'ADMITIDOS', 'AÑO']
+
+# Seleccionar solo las columnas que te interesan del DataFrame de admitidos
 df_admitidos_2023 = df_admitidos_2023[columnas_admitidos]
-columnas_matriculados = ['CÓDIGO DE LA INSTITUCIÓN','CÓDIGO SNIES DEL PROGRAMA','ID ÁREA','ID SEXO','AÑO','SEMESTRE','MATRICULADOS']
+
+# Asegurarse de que las columnas tienen los tipos de datos correctos
+df_admitidos_2023['ADMITIDOS'] = df_admitidos_2023['ADMITIDOS'].astype(int)
+df_admitidos_2023['ID MODALIDAD'] = df_admitidos_2023['ID MODALIDAD'].astype(int)
+df_admitidos_2023['CÓDIGO DE LA INSTITUCIÓN'] = df_admitidos_2023['CÓDIGO DE LA INSTITUCIÓN'].astype(int)
+df_admitidos_2023['AÑO'] = df_admitidos_2023['AÑO'].astype(int)
+
+# Renombrar las columnas para que coincidan con los nombres que deseas insertar
+df_admitidos_2023.rename(columns={
+    'CÓDIGO DE LA INSTITUCIÓN': 'ID_INSTITUCION',
+    'ID MODALIDAD': 'ID_MODALIDAD',
+    'ADMITIDOS': 'ADMITIDOS',
+    'AÑO': 'ANIO'
+}, inplace=True)
+
+# Mostrar el DataFrame para verificar
+print(df_admitidos_2023)
+# Llamar a la función para insertar el DataFrame de admitidos
+base_datos.insert_snies_fact(df=df_admitidos_2023, columnas=df_admitidos_2023.columns)
+
+# Filtrar el DataFrame para que contenga las columnas necesarias
+columnas_graduados = ['GRADUADOS', 'CÓDIGO DE LA INSTITUCIÓN', 'CÓDIGO DEL DEPARTAMENTO (PROGRAMA)', 'CÓDIGO SNIES DEL PROGRAMA', 'AÑO']
+
+# Seleccionar las columnas necesarias del DataFrame
+df_graduados_2023 = df_graduados_2023[columnas_graduados]
+
+# Asegurarse de que las columnas tienen los tipos de datos correctos
+df_graduados_2023['GRADUADOS'] = df_graduados_2023['GRADUADOS'].astype(int)
+df_graduados_2023['CÓDIGO DE LA INSTITUCIÓN'] = df_graduados_2023['CÓDIGO DE LA INSTITUCIÓN'].astype(int)
+df_graduados_2023['CÓDIGO DEL DEPARTAMENTO (PROGRAMA)'] = df_graduados_2023['CÓDIGO DEL DEPARTAMENTO (PROGRAMA)'].astype(int)
+df_graduados_2023['CÓDIGO SNIES DEL PROGRAMA'] = df_graduados_2023['CÓDIGO SNIES DEL PROGRAMA'].astype(int)
+df_graduados_2023['AÑO'] = df_graduados_2023['AÑO'].astype(int)
+
+
+# Renombrar las columnas para que coincidan con los nombres que deseas insertar
+df_graduados_2023.rename(columns={
+    'GRADUADOS': 'ID_GRADUADO',
+    'AÑO': 'FECHA_GRADUACION',
+    'CÓDIGO DE LA INSTITUCIÓN': 'ID_INSTITUCION',
+    'CÓDIGO DEL DEPARTAMENTO (PROGRAMA)': 'ID_DEPARTAMENTO',
+    'CÓDIGO SNIES DEL PROGRAMA': 'ID_ACREDITACION'
+}, inplace=True)
+
+# Mostrar el DataFrame para verificar
+print(df_graduados_2023)
+# Llamar a la función para insertar el DataFrame de graduados
+base_datos.insert_graduados_fact(df=df_graduados_2023, columnas=df_graduados_2023.columns)
+
+############################################################################################################################
+
+# Filtrar el DataFrame para que contenga las columnas necesarias de inscritos
+columnas_inscritos = ['INSCRITOS', 'ID SEXO', 'ID NIVEL DE FORMACIÓN', 'CÓDIGO SNIES DEL PROGRAMA', 'AÑO']
+
+# Seleccionar solo las columnas necesarias del DataFrame
+df_inscritos_2023 = df_inscritos_2023[columnas_inscritos]
+
+# Asegurarse de que las columnas tengan los tipos de datos correctos
+df_inscritos_2023['INSCRITOS'] = df_inscritos_2023['INSCRITOS'].astype(int)
+df_inscritos_2023['ID SEXO'] = df_inscritos_2023['ID SEXO'].astype(int)
+df_inscritos_2023['ID NIVEL DE FORMACIÓN'] = df_inscritos_2023['ID NIVEL DE FORMACIÓN'].astype(int)
+df_inscritos_2023['CÓDIGO SNIES DEL PROGRAMA'] = df_inscritos_2023['CÓDIGO SNIES DEL PROGRAMA'].astype(int)
+df_inscritos_2023['AÑO'] = df_inscritos_2023['AÑO'].astype(int)
+
+# Renombrar las columnas para que coincidan con los nombres que deseas insertar
+df_inscritos_2023.rename(columns={
+    'INSCRITOS': 'ID_INSCRITOS',
+    'AÑO': 'FECHA_INSCRIPCION',
+    'ID SEXO': 'ID_SEXO',
+    'ID NIVEL DE FORMACIÓNn': 'ID_FORMACION',
+    'CÓDIGO SNIES DEL PROGRAMA': 'ID_PROGRAMA_ACADEMICO'
+}, inplace=True)
+
+# Mostrar el DataFrame para verificar
+print(df_inscritos_2023)
+
+# Llamar a la función para insertar el DataFrame de inscritos
+base_datos.insert_inscritos_fact(df=df_inscritos_2023, columnas=df_inscritos_2023.columns)
+'''
+#########################################################################################################################
+
+# Filtrar el DataFrame para que contenga las columnas necesarias de matriculados
+columnas_matriculados = ['MATRICULADOS', 'CÓDIGO SNIES DEL PROGRAMA', 'CÓDIGO DE LA INSTITUCIÓN', 'AÑO']
+
+# Seleccionar solo las columnas necesarias del DataFrame
 df_matriculados_2023 = df_matriculados_2023[columnas_matriculados]
 
-columnas_clave = ['CÓDIGO DE LA INSTITUCIÓN','CÓDIGO SNIES DEL PROGRAMA','ID ÁREA','ID SEXO','AÑO','SEMESTRE']
+# Asegurarse de que las columnas tengan los tipos de datos correctos
+df_matriculados_2023['MATRICULADOS'] = df_matriculados_2023['MATRICULADOS'].astype(int)
+df_matriculados_2023['CÓDIGO SNIES DEL PROGRAMA'] = df_matriculados_2023['CÓDIGO SNIES DEL PROGRAMA'].astype(int)
+df_matriculados_2023['CÓDIGO DE LA INSTITUCIÓN'] = df_matriculados_2023['CÓDIGO DE LA INSTITUCIÓN'].astype(int)
+df_matriculados_2023['AÑO'] = df_matriculados_2023['AÑO'].astype(int)
 
-df_fact = pd.merge(df_admitidos_2023, df_matriculados_2023, on=columnas_clave, how='inner')
-df_fact['MATRICULADOS'] = df_fact['MATRICULADOS'].astype(int)
-df_fact['ADMITIDOS'] = df_fact['ADMITIDOS'].astype(int)
-df_fact['ID ÁREA'] = df_fact['ID ÁREA'].astype(int)
-df_fact['ID SEXO'] = df_fact['ID SEXO'].astype(int)
-df_fact['AÑO'] = df_fact['AÑO'].astype(int)
-df_fact['SEMESTRE'] = df_fact['SEMESTRE'].astype(int)
-df_fact['CÓDIGO SNIES DEL PROGRAMA'] = df_fact['CÓDIGO SNIES DEL PROGRAMA'].astype(int)
-print(df_fact)
-base_datos.insert_snies_fact(df=df_fact,columnas=df_fact.columns)
-'''
+# Renombrar las columnas para que coincidan con los nombres que deseas insertar
+df_matriculados_2023.rename(columns={
+    'MATRICULADOS': 'ID_MATRICULADOS',
+    'CÓDIGO SNIES DEL PROGRAMA': 'ID_PROGRAMA_ACADEMICO',
+    'CÓDIGO DE LA INSTITUCIÓN': 'ID_INSTITUCION',
+    'AÑO': 'ID_ANIO'
+}, inplace=True)
+
+# Mostrar el DataFrame para verificar
+print(df_matriculados_2023)
+
+# Llamar a la función para insertar el DataFrame de matriculados
+base_datos.insert_matriculados_fact(df=df_matriculados_2023, columnas=df_matriculados_2023.columns)
+
+
